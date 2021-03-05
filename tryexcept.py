96b2a95b6565b089033e7ab0a809c80400a0cbf5
CarlosJardonIglesias <carlosjardon12@gmail.com>
2021-03-05 17:40:56 +0100

    try except example

---
+++ tryexcept.py
@@ -0,0 +1,17 @@
+while True:
+    try:
+        # first we read the age as characters
+        age = input("How old are you?")
+        # we try to convert the age to the number
+        age = int(age)
+    except ValueError:
+        print("Sorry, but that was not a number")
+    except ZeroDivisionError:
+        print("Can not divide by zero")
+    except:
+        print("Something else happened")
+    else:
+        print("you were born around", 2021 - age)
+        break
+    finally:
+        print("Thanks for playing my game")
